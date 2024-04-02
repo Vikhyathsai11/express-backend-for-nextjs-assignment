8c7fda68dcff324e04858cd46f41ab9026fe7ee0
Vikhyathsai11 <vikhyathsai11@gmail.com>
2024-04-02 15:22:54 +0530

    api working now

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors =require('cors');
 
 const app = express();
 const port = 3002;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 // similar to flask rerouting type this is being used 
